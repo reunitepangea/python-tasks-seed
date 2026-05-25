f27f07cdf271a3602e20f6839844142102beaadf
Maria Andrianova <andrianowamaar@yandex.ru>
2026-05-25 15:06:41 +0300

    Update sorting_demo.py

--- 01.InplaceSorting.s/sorting_demo.py
+++ 01.InplaceSorting.s/sorting_demo.py
@@ -5,9 +5,18 @@ Sortings demo
 
 from comp_swap_container import CompSwapList
 import sortings
+import random
 
 if __name__ == "__main__":
     # trivial_sort2
     data: CompSwapList[int] = CompSwapList([2, 1])
     sortings.trivial_sort2(data)
     print(f"Comps: {data.comps}, Swaps: {data.swaps}")
+    for n in [10, 100, 1000]:
+        data = CompSwapList(random.sample(range(n), n))
+        sortings.selection_sort(data)
+        print(f"selection_sort n={n}: comps={data.comps}, swaps={data.swaps}")
+
+        data = CompSwapList(random.sample(range(n), n))
+        sortings.quick_sort(data)
+        print(f"quick_sort     n={n}: comps={data.comps}, swaps={data.swaps}")

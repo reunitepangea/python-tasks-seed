3111a0dd40835b64ccf227f71378e7d70dd371cf
Maria Andrianova <andrianowamaar@yandex.ru>
2026-05-25 15:06:16 +0300

    Update sortings.py

--- 01.InplaceSorting.s/sortings.py
+++ 01.InplaceSorting.s/sortings.py
@@ -5,6 +5,7 @@ Sorting algorithms
 from __future__ import annotations
 from typing import Any
 from comp_swap_container import CompSwapList
+import random
 
 
 def trivial_sort2(data: CompSwapList[Any]):
@@ -20,3 +21,29 @@ def trivial_sort2(data: CompSwapList[Any]):
         raise ValueError("Expected at most 2 elements!")
     if data.less(1, 0):
         data.swap(0, 1)
+
+def quick_sort(data: CompSwapList[Any], lo: int = 0, hi: int = None):
+    if hi is None:
+        hi = len(data) - 1
+    if lo >= hi:
+        return
+    pivot_idx = random.randint(lo, hi)
+    data.swap(pivot_idx, hi)
+    i = lo
+    for j in range(lo, hi):
+        if data.less(j, hi):
+            data.swap(i, j)
+            i += 1
+    data.swap(i, hi)
+    quick_sort(data, lo, i - 1)
+    quick_sort(data, i + 1, hi)
+
+def selection_sort(data: CompSwapList[Any]):
+    n = len(data)
+    for i in range(n-1):
+        mn = i
+        for j in range(i+1, n):
+            if data.less(j, mn):
+                mn = j
+        if mn != i:
+            data.swap(i, mn)

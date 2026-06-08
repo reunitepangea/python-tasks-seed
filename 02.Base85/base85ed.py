11d12fea561cd593ae77880b9c6ac6760eaa842b
Maria Andrianova <andrianowamaar@yandex.ru>
2026-06-08 15:43:09 +0300

    Update base85ed.py

--- 02.Base85/base85ed.py
+++ 02.Base85/base85ed.py
@@ -8,15 +8,15 @@ from beartype import beartype
 
 
 @beartype
-def encode(b:bytes):
-    alphabet = b'0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&()*+-;<=>?@^_`{|}~'
+def encode(b: bytes):
+    alphabet = b"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&()*+-;<=>?@^_`{|}~"
     result = []
     i = 0
     if len(b) == 0:
         return b""
     while i + 4 <= len(b):
-        block = b[i:i+4]
-        number = block[0]*(pow(256, 3)) + block[1]*(pow(256, 2)) + block[2]*256 + block[3]
+        block = b[i : i + 4]
+        number = block[0] * (pow(256, 3)) + block[1] * (pow(256, 2)) + block[2] * 256 + block[3]
         digits = []
         for j in range(5):
             digits.append(number % 85)
@@ -29,13 +29,13 @@ def encode(b:bytes):
         end = b[i:]
         len_end = len(end)
         end = end + b'\x00'*(4 - len_end)
-        number = end[0]*(pow(256, 3)) + end[1]*(pow(256, 2)) + end[2]*256 + end[3]
+        number = end[0] * (pow(256, 3)) + end[1] * (pow(256, 2)) + end[2] * 256 + end[3]
         digits = []
         for j in range(5):
             digits.append(number % 85)
             number = number // 85
         digits = digits[::-1]
-        digits = digits[0:len_end+1]
+        digits = digits[0:len_end + 1]
         for digit in digits:
             result.append(alphabet[digit])
     return bytes(result)
@@ -44,7 +44,7 @@ def encode(b:bytes):
 @beartype
 def decode(b: bytes):
     result = []
-    alphabet = b'0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&()*+-;<=>?@^_`{|}~'
+    alphabet = b"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&()*+-;<=>?@^_`{|}~"
     i = 0
     digits = []
     for char in b:
@@ -52,8 +52,8 @@ def decode(b: bytes):
     if len(digits) == 0:
         return b""
     while i + 5 <= len(digits):
-        block = digits[i:i+5]
-        number = block[0] * (pow(85, 4)) + block[1] * (pow(85, 3)) + block[2] * (pow(85,2)) + block[3]*85 + block[4]
+        block = digits[i:i + 5]
+        number = block[0] * (pow(85, 4)) + block[1] * (pow(85, 3)) + block[2] * (pow(85,2)) + block[3] * 85 + block[4]
         bbytes = []
         for j in range(4):
             bbytes.append(number % 256)
@@ -69,7 +69,7 @@ def decode(b: bytes):
             raise ValueError
         for j in range(5 - len_end):
             end.append(84)
-        number = end[0] * (pow(85, 4)) + end[1] * (pow(85, 3)) + end[2] * (pow(85,2)) + end[3]*85 + end[4]
+        number = end[0] * (pow(85, 4)) + end[1] * (pow(85, 3)) + end[2] * (pow(85,2)) + end[3] * 85 + end[4]
         bbytes = []
         for j in range(4):
             bbytes.append(number % 256)
